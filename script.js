60717e5a185250acf2e4162a8ad28864c3a663b9
Kamalpreet408 <163439294+Kamalpreet408@users.noreply.github.com>
2024-04-27 21:25:56 -0600

    css files removed and placed into folder
    css

--- script.js
+++ script.js
@@ -16,24 +16,3 @@ $(document).ready(function(){
       }
     });
   });
-
-
-let images = document.querySelectorAll("img");
-let wrapper = document.getElementById("wrapper");
-let imgWrapper = document.getElementById("fullImg");
-let close = document.querySelector("span");
-
-images.forEach((img, index) => {
-  img.addEventListener("click", () => {
-  });
-});
-
-close.addEventListener("click", () => {
-  console.log("clicked");
-  wrapper.style.display = "none";
-});
-
-function openModal(pic) {
-  wrapper.style.display = "flex";
-  imgWrapper.src = pic;
-}
